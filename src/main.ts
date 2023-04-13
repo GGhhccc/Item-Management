f20e81b566fa4f6be41e8272a73a6774d3ffd6ea
dd <593274462@qq.com>
2023-04-13 17:00:08 +0800

    chore: 引入 pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createSSRApp } from "vue";
 import uView from "uview-ui";
 import App from "./App.vue";
+import * as Pinia from 'pinia'
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(Pinia.createPinia());
   return {
     app,
+	Pinia,
   };
 }
--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -6,7 +6,8 @@
 </template>
 
 <script setup lang="ts">
-
+import { useUserStore } from '@/stores/user';
+const user = useUserStore()
 </script>
 
 <style>
